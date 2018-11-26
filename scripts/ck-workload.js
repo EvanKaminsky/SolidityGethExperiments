cc113be21b157c4f84a638498e91224de79bcf82
Evan Kaminsky <evanskaminsky@gmail.com>
2018-11-25 21:33:36 -0600

    updated workload with individual contract miners

--- scripts/ck-workload.js
+++ scripts/ck-workload.js
@@ -35,19 +35,42 @@ function kittyStats() {
 	console.log();
 }
 
+
+GAS_ACCOUNT = eth.accounts[0];
+
+function unlock() {
+	personal.unlockAccount(GAS_ACCOUNT, "1234");
+}
+
 function allStats() {
 	stats();
 	kittyStats();
 }
 
+function mineThenStats() {
+	mineWhilePending();
+	allStats();
+}
+
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+  		if ((new Date().getTime() - start) > milliseconds){
+    		break;
+  		}
+  	}
+}
+
+
+
 
 
 // Transfer
 
-function _sendTransfer(from_account_index, to_account_index, kitty_id, gas, gas_account, print) {
+function _sendTransfer(from_account_index, to_account_index, kitty_id, gas, print) {
 	from_account = (from_account_index < 0) ? 0 : eth.accounts[from_account_index];
 	to_account   = (  to_account_index < 0) ? 0 : eth.accounts[  to_account_index];
-	personal.unlockAccount(gas_account, "1234");
+	unlock();
 
 	// Generate 'trsansferFrom' bytecode
 	transaction_abi = KittyCore.transferFrom.getData(from_account, to_account, kitty_id);
@@ -56,30 +79,24 @@ function _sendTransfer(from_account_index, to_account_index, kitty_id, gas, gas_
 	}
 
 	// Send transfer transaction to the blockchain
-	eth.sendTransaction({from: gas_account, to: KittyCore.address, data: transaction_abi, gas: gas});
+	eth.sendTransaction({from: GAS_ACCOUNT, to: KittyCore.address, data: transaction_abi, gas: gas});
 	if (print) {
 		stats();
 	}
 }
 
-
-function transferWithPayer(from_account_index, to_account_index, kitty_id, gas, gas_account) {
-	_sendTransfer(from_account_index, to_account_index, kitty_id, gas, gas_account, true);
-	mineWhilePending();
-	allStats();
-}
-
 function transfer(from_account_index, to_account_index, kitty_id, gas) {
-	transferWithPayer(from_account_index, to_account_index, kitty_id, gas, eth.accounts[0]);
+	_sendTransfer(from_account_index, to_account_index, kitty_id, gas, true);
+	mineThenStats();
 }
 
 
 
 // Create
 
-function _sendCreate(mom_id, dad_id, gen, genes, owner_account_index, gas, gas_account, print) {
+function _sendCreate(mom_id, dad_id, gen, genes, owner_account_index, gas, print) {
 	owner_account = (owner_account_index < 0) ? 0 : eth.accounts[owner_account_index];
-	personal.unlockAccount(gas_account, "1234");
+	unlock();
 	
 	// Generate 'createKitty' bytecode 
 	create_abi = KittyCore.createKitty.getData(mom_id, dad_id, gen, genes, owner_account);
@@ -88,20 +105,15 @@ function _sendCreate(mom_id, dad_id, gen, genes, owner_account_index, gas, gas_a
 	}
 
 	// Send transfer transaction to the blockchain
-	eth.sendTransaction({from: gas_account, to: KittyCore.address, data: create_abi, gas: gas});
+	eth.sendTransaction({from: GAS_ACCOUNT, to: KittyCore.address, data: create_abi, gas: gas});
 	if (print) {
 		stats();
 	}
 }
 
-function createWithPayer(mom_id, dad_id, gen, genes, owner_account_index, gas, gas_account) {
-	_sendCreate(mom_id, dad_id, gen, genes, owner_account_index, gas, gas_account, true);
-	mineWhilePending();
-	allStats();
-}
-
 function create(owner_account_index, gas) {
-	createWithPayer(0, 0, 0, 0, owner_account_index, gas, eth.accounts[0]);
+	_sendCreate(0, 0, 0, 0, owner_account_index, gas, true);
+	mineThenStats();
 }
 
 
@@ -119,9 +131,71 @@ function multipleTransfers(quantity, gas) {
 	}
 
 	stats();
-	mineWhilePending();
-	allStats();
+	mineThenStats();
 }
+
+function multipleCreates(quantity, gas) {
+	allStats();
+
+	for (var i = 0; i < quantity; i++) {
+		_sendCreate(0, 0, 0, 0, 0, gas, eth.accounts[0], false);
+	}
+
+	stats();
+	mineThenStats();
+}
+
+
+
+// Gas Calculations
+
+// Copy & Paste this function - Eval requires global scope with Geth
+function scopifyContracts() {
+	var contracts = contractOutput.contracts;
+	unlock();
+
+	for (var key in contracts) {
+		var contract_name = key.split(":")[1];
+		var abi = JSON.parse(contracts[key].abi);
+		var bin = "0x" + contracts[key].bin;
+		
+		var contract = web3.eth.contract(abi);
+		eval("var " + contract_name + "Bin = bin;");
+		eval("var " + contract_name + "Contract = contract;");
+	}
+}
+
+function submitContract(name, gas) {
+	unlock();
+
+	eval("var bin = " + name + "Bin;");
+	eval("var contract = " + name + "Contract;");
+
+	var storage_instance = contract.new({from: GAS_ACCOUNT, data: bin, gas: gas}, function(err, contract) {
+   		if (err) {
+   			console.log("✕ " + name + "\n   " + err);
+   		} else if (!contract.address) {
+   	 		console.log("~ " + name + " Pending\n");
+  		} else {
+     		console.log("✓ " + name + "Contract successfully mined!\n   Contract Address: " + contract.address);
+    	}
+    });
+
+	eval("var " + name + "Storage = storage_instance;");
+}
+
+function submitMineContract(name) {
+	miner.start(1);
+	sleep(1000 * 60 * 1);
+	miner.stop();
+
+	stats();
+	submitContract(name, 1100000);
+	stats();
+	mineWhilePending();
+	stats();
+}
+
 
 
 /*
